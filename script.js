d6104cfc3bc122b6c2baa7de545f53d4aec9bdff
DarrenXu94 <DarrenXu1994@yahoo.com.au>
2016-02-24 12:37:07 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+var canvas = document.getElementById("mainCanvas");
+var context = canvas.getContext("2d");
+
+var keys = [];
+
+var width = 500, height = 400, speed = 4;
+
+var score = 0;
+
+var player = {
+	x: 40,
+	y: 40,
+	width: 20,
+	height: 20
+};
+
+var cube = {
+	x: Math.random() * (width - 20),
+	y: Math.random() * (height - 20), /* cube.height*/
+	width: 20,
+	height: 20
+};
+
+window.addEventListener("keydown", function(e){
+	keys[e.keyCode] = true;
+}, false);
+
+window.addEventListener("keyup", function(e){
+	delete keys[e.keyCode];
+}, false);
+
+
+/* up 38 down 40 left 37 right 39 */
+
+function game(){
+	update();
+	render();
+}
+
+function update(){
+	if(keys[38]) player.y-=speed;
+	if(keys[40]) player.y+=speed;
+	if(keys[37]) player.x-=speed;
+	if(keys[39]) player.x+=speed;
+
+	if(player.x < 0) player.x = 0;
+	if(player.y < 0) player.y = 0;
+	if(player.x >= width - player.width) player.x = width - player.width;
+	if(player.y >= height - player.height) player.y = height - player.height;
+
+	if (collision(player, cube)) process();
+	mouseObject();
+}
+
+function render(){
+	context.clearRect(0,0,width,height);
+	
+	context.fillStyle = "blue";
+	context.fillRect(player.x,player.y, player.width, player.height);
+
+	context.fillStyle = "red";
+	context.fillRect(cube.x, cube.y, cube.width, cube.height);
+
+	context.font = "bold 30px helvetica";
+	context.fillStyle = "black";
+	context.fillText(score, 10, 30);
+}
+
+function collision(first, second){
+	return !(first.x > second.x + second.width || 
+first.x + first.width < second.x || 
+first.y > second.y + second.height ||
+first.y + first.height < second.y);
+
+}
+
+function process(){
+	score++;
+	cube.x = Math.random() * (width - 20);
+	cube.y = Math.random() * (height - 20);
+}
+
+setInterval(function(){
+	game();
+}, 1000/30);
